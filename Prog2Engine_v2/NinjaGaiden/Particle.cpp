c98e32cc18bf5ffd9bb13f63336370f33ddb930b
Ivans Minajevs <ivans.minajevs@student.howest.be>
2024-05-02 14:43:27 +0200

    Implemeted Particles and ParticlesManager

---
+++ Prog2Engine_v2/NinjaGaiden/Particle.cpp
@@ -0,0 +1,58 @@
+﻿#include "pch.h"
+#include "Particle.h"
+#include "Texture.h"
+
+DeathParticle::DeathParticle( Point2f pos ) :
+		m_Position(pos)
+{
+	m_DeathParticleSpriteSheetPtr = new Texture(m_FILE_PATH);
+	InitSourceRect();
+	
+	m_AccuSec = 0;
+	m_FramesPerSec = m_FRAMES_OF_ANIMATION;
+	m_FrameTime	= 1.f / m_FramesPerSec;
+	m_FrameNr = 0;
+};
+
+void DeathParticle::InitSourceRect()
+{
+	m_SourceRect.width  = m_DeathParticleSpriteSheetPtr->GetWidth() / m_FRAMES_OF_ANIMATION;
+	m_SourceRect.height = m_DeathParticleSpriteSheetPtr->GetHeight();
+	UpdateSourceRect();
+}
+
+void DeathParticle::UpdateSourceRect()
+{
+	m_SourceRect.left = float(m_FrameNr % m_FRAMES_OF_ANIMATION) * m_DeathParticleSpriteSheetPtr->GetWidth() / m_FRAMES_OF_ANIMATION;
+	m_SourceRect.bottom = 0.f;
+}
+
+void DeathParticle::Update(float elapsedSec)
+{
+	ChangeFrames(elapsedSec);
+	UpdateSourceRect();
+}
+
+void DeathParticle::Draw() const
+{
+	glPushMatrix();
+		glTranslatef(m_Position.x, m_Position.y, 0.f);
+		m_DeathParticleSpriteSheetPtr->Draw(Point2f(), m_SourceRect);
+	glPopMatrix();
+}
+
+void DeathParticle::ChangeFrames(float elapsedSec)
+{
+	m_AccuSec += elapsedSec;
+	if (m_AccuSec >= m_FrameTime)
+	{
+		m_AccuSec-= m_FrameTime;
+		++m_FrameNr;
+		
+		if (m_FrameNr >= m_FRAMES_OF_ANIMATION)
+		{
+			m_FrameNr = 0;
+		}
+	}
+}
+
---
+++ Prog2Engine_v2/NinjaGaiden/Particle.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include "Texture.h"
+
+class DeathParticle
+{
+public:
+	DeathParticle(Point2f pos);
+
+	void Draw() const;
+	void Update(float elpasedSec);
+
+	const int m_FRAMES_OF_ANIMATION{ 5 };
+	//const float m_FRAME_SIZE { 48 };
+	const std::string m_FILE_PATH {"death_particle.png"};
+private:
+	Texture* m_DeathParticleSpriteSheetPtr;
+	Point2f m_Position;
+	Rectf m_SourceRect;
+
+	int m_FrameNr;
+	float m_AccuSec;
+	int	  m_FramesPerSec;
+	float m_FrameTime;
+
+	void InitSourceRect();
+	void UpdateSourceRect();
+	//void ChangeMaxFramesOfAnimation();
+	void ChangeFrames(float elapsedSec);
+};
